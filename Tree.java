3b0c6a0fecf4db3434deedda16e0d40151db8198
lihao <shuohanli1025@gmail.com>
2023-04-15 00:47:13 +0900

    no message

---
+++ Tree.java
@@ -0,0 +1,20 @@
+// Define your tree class in this file!
+  public class Tree {
+
+  public static void main(String[]args) {
+
+    //你们好，我叫李好
+
+    
+    System.out.println("Let me plant a tree");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+  }
+  
+   }
