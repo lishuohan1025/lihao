e037c97d0f18f4710f8ef1b2ca477131ebf8fe62
lishuohan1025 <123968789+lishuohan1025@users.noreply.github.com>
2023-05-15 11:36:14 +0900

    Add files via upload

---
+++ BitOperator.java
@@ -0,0 +1,20 @@
+public class BitOperator {
+	
+	public static void main(String[] args) {
+		
+		
+		int a = 1 >> 2;
+		//00000000 00000000 00000000 00000001
+		
+		//00000000 00000000 00000000 00000001 = >00000000 本质 1 / 2 / 2 = 0
+		
+		int c = 1 << 2;
+		//1 = > 00000001 = > 00000010 本质 1 * 2  *2 = 4
+		
+		System.out.println(1 >> 2);//0
+		System.out.println(1 << 2);//4
+		
+		
+		
+	}
+}
